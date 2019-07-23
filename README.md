f5e426fc9c0ec07d36335c781b61387ad584255f
Kyrylo Gerasymenko <kyrylo.gerasymenko@globallogic.com>
2019-07-23 15:54:33 +0300

    adds simple webhook

--- README.md
+++ README.md
@@ -18,3 +18,28 @@ https://www.freedesktop.org/software/systemd/man/systemd.exec.html#
 - simple server w/o express
 - reading nodejs server/request/response documentation
 - require('util').debuglog, NODE_DEBUG=...
+- eslint integration with prettier
+- project-level VScode settings
+
+Code to create webhook manually:
+
+```
+curl -X "POST" 'https://api.trello.com/1/tokens/[TOKEN]/webhooks/?key=[KEY]&idModel=5ccadbf8b2690760ce8f5912&description="My Webhook"&callbackURL=http://moydomen.com:2018/trello'
+
+{
+    "id": "5d362731b3086719fb688de6",
+    "description": "My Webhook",
+    "idModel": "5ccadbf8b2690760ce8f5912",
+    "callbackURL": "http://moydomen.com:2018/trello",
+    "active": true
+}
+```
+
+## Hints:
+
+- run `npm run env:decrypt <password>` to recover .env file from encrypted version
+- run `npm run env:encrypt <password>` to re-generate .env.enc file from .env file
+
+## TODO
+
+- Make server include request path (e.g. /trello -> trello) into event name
--- hook-subscriber.js
+++ hook-subscriber.js
@@ -1,9 +1,36 @@
 require('dotenv').config()
 const cote = require('cote')
+const request = require('request')
 
 console.log(`Redis: ${process.env.COTE_DISCOVERY_REDIS_HOST}`)
+const TRELLO_KEY = process.env.TRELLO_KEY
+const TRELLO_TOKEN = process.env.TRELLO_TOKEN
 
+var ignoreNextMove = false
+
+// this subscriber prevents cards from being moved to the other list
 const subscriber = new cote.Subscriber({ name: 'Web-hook subscriber' })
 subscriber.on('POST hook received', update => {
-  console.log(update)
+  const display = update.action.display
+  if (display.translationKey !== 'action_move_card_from_list_to_list') return
+  console.log(
+    display,
+    display.translationKey === 'action_move_card_from_list_to_list',
+    display.entities.card.id,
+    display.entities.listBefore.id,
+    display.entities.listAfter.id
+  )
+  const options = {
+    method: 'PUT',
+    url: `https://api.trello.com/1/cards/${display.entities.card.id}?key=${TRELLO_KEY}&idList=${display.entities.listBefore.id}&token=${TRELLO_TOKEN}`,
+  }
+  if (ignoreNextMove) {
+    ignoreNextMove = false
+  } else {
+    ignoreNextMove = true
+    request(options, function(error, response, body) {
+      if (error) throw new Error(error)
+      // console.log(body)
+    })
+  }
 })
